2d6717bbfa17a68d470213399d5d4fc5e5bb93d3
Hezam.Ali <hezam1404@gmail.com>
2023-06-05 22:14:01 +0300

    2 task

---
+++ 2task-docker-image/config.php
@@ -0,0 +1,8 @@
+<?php
+// MySQL database configuration
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_NAME', 'your_database_name');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+?>
